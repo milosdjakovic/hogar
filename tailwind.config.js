f498c76082c1242960e6f927909c5c39f39e1e6c
Milos Djakovic <milos.g.djakovic@gmail.com>
2021-08-03 17:54:36 +0200

    All outdated dependencies updated and Tailwind migrated to V2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,34 @@
 module.exports = {
+  mode: "jit",
   purge: {
     // enabled: true,
-    content: ["./src/**/*.html", "./src/**/*.jsx", "./src/**/*.js"]
+    content: ["./src/**/*.html", "./src/**/*.jsx", "./src/**/*.js"],
   },
   theme: {
     extend: {
       boxShadow: {
         "bottom-orange": "0 2px 0 rgba(221, 107, 32)",
       },
+      colors: {
+        orange: {
+          100: "#fffaf0",
+          200: "#feebc8",
+          300: "#fbd38d",
+          400: "#f6ad55",
+          500: "#ed8936",
+          600: "#dd6b20",
+          700: "#c05621",
+          800: "#9c4221",
+          900: "#7b341e",
+        },
+      },
     },
     screens: {
-      'xs': '380px',
-      'sm': '640px',
-      'md': '768px',
-      'lg': '1024px',
-      'xl': '1280px',
+      xs: "380px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
     },
     inset: {
       "0": 0,
