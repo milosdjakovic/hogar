6870536629301824bf865776ceaa1db5e11c8158
Milos Djakovic <milos.g.djakovic@gmail.com>
2021-08-09 01:02:41 +0200

    Header link hover styles changed

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -14,13 +14,13 @@ const Header = () => {
       style={{ backdropFilter: "blur(4px)", WebkitBackdropFilter: "blur(4px)" }}
     >
       <div className="flex justify-between w-full h-12 max-w-4xl px-2 sm:px-6">
-        <nav className="flex text-gray-600 font-patrick text-2xl">
+        <nav className="flex font-patrick text-2xl text-gray-700">
           <Link
             style={{
               transition: "background 300ms, color 300ms, box-shadow 300ms",
             }}
-            className="flex items-center h-full px-4 hover:shadow-bottom-orange hover:bg-orange-600 hover:text-orange-100"
-            activeClassName="shadow-bottom-orange bg-orange-600 text-orange-100"
+            className="flex items-center h-full px-4 hover:shadow-bottom-orange hover:bg-orange-200"
+            activeClassName="shadow-bottom-orange bg-orange-600 text-orange-100 hover:text-white"
             to="/"
           >
             <span className="whitespace-nowrap">Hogar 1</span>
@@ -29,8 +29,8 @@ const Header = () => {
             style={{
               transition: "background 300ms, color 300ms, box-shadow 300ms",
             }}
-            className="flex items-center h-full px-4 hover:shadow-bottom-orange hover:bg-orange-600 hover:text-orange-100"
-            activeClassName="shadow-bottom-orange bg-orange-600 text-orange-100"
+            className="flex items-center h-full px-4 hover:shadow-bottom-orange hover:bg-orange-200"
+            activeClassName="shadow-bottom-orange bg-orange-600 text-orange-100 hover:text-white"
             to="/2/"
           >
             <span className="whitespace-nowrap">Hogar 2</span>
