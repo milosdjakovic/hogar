cfb1fee3820dfacf4bf51a76d9a2da40cee54b5f
Milos Djakovic <milos.g.djakovic@gmail.com>
2021-08-08 16:14:08 +0200

    404 page UI update

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -13,7 +13,7 @@ const NotFound = () => {
       <SEO title="Strana nije pronađena" />
 
       <div
-        className="min-h-screen p-10 flex flex-col items-center"
+        className="min-h-screen py-6 px-10 flex flex-col items-center"
         style={{
           backgroundImage: `url(${bricks})`,
           backgroundAttachment: "fixed",
@@ -21,10 +21,11 @@ const NotFound = () => {
       >
         <HogarLogo className="w-full max-h-[40vh]" />
 
-        <h1 className="mt-8 text-4xl xs:text-6xl font-patrick text-center leading-7 xs:leading-[3rem]">
+        <h1 className="mt-4 text-4xl sm:text-6xl font-patrick text-center leading-7 xs:leading-[3rem]">
           Strana nije pronađena
         </h1>
-        <div className="text-3xl mt-6 flex whitespace-nowrap flex-col sm:flex-row items-baseline sm:gap-4  font-patrick">
+
+        <div className="text-3xl mt-2 flex whitespace-nowrap flex-col items-center sm:flex-row sm:items-baseline gap-4 font-patrick">
           <p className="text-gray-700">Posetite</p>
           <Link
             to="/"
