c56439b22baa6b2c9820dce48e73ba886dac36b7
Milos Djakovic <milos.g.djakovic@gmail.com>
2021-08-08 15:04:26 +0200

    Sidebar Navigation: Removed underline when the link is hovered, and code refactored

--- src/components/sidebarNav.jsx
+++ src/components/sidebarNav.jsx
@@ -1,26 +1,38 @@
 import React from "react"
-import { useSelector } from 'react-redux'
+import { useSelector } from "react-redux"
 
 import TearedEdge from "./tearedSide"
-import linedPaper from '../images/lined_paper.png'
+import linedPaper from "../images/lined_paper.png"
 
 const SidebarNav = ({ categories }) => {
-  const menuItemInViewport = useSelector(state => state.menuItemInViewport);
+  const menuItemInViewport = useSelector(state => state.menuItemInViewport)
 
   const SidebarLink = ({ category }) => (
     <a
       href={`#${category}`}
-      className={`${menuItemInViewport === category && `text-orange-600 font-bold`} py-1 flex text-gray-700 hover:text-orange-600 hover:underline`}
-      style={{ marginLeft: menuItemInViewport === category && '-32px' }}
+      className={`${
+        menuItemInViewport === category && `text-orange-600 font-bold`
+      } py-1 flex text-gray-700 hover:text-orange-600`}
+      style={{ marginLeft: menuItemInViewport === category && "-32px" }}
     >
-      {
-        menuItemInViewport === category
-        &&
-        <svg xmlns="http://www.w3.org/2000/svg" style={{ marginRight: "7px" }} className="inline icon icon-tabler icon-tabler-point" width="24" height="24" viewBox="0 0 24 24" strokeWidth="3" stroke="rgba(237, 137, 54)" fill="none" strokeLinecap="round" strokeLinejoin="round">
+      {menuItemInViewport === category && (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          style={{ marginRight: "7px" }}
+          className="text-orange-600 inline icon icon-tabler icon-tabler-point"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          strokeWidth="3"
+          stroke="currentColor"
+          fill="none"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        >
           <path stroke="none" d="M0 0h24v24H0z" />
           <circle cx="12" cy="12" r="4" />
         </svg>
-      }
+      )}
       {category.charAt(0).toUpperCase() + category.slice(1)}
     </a>
   )
@@ -28,7 +40,7 @@ const SidebarNav = ({ categories }) => {
   return (
     <div
       className="sticky flex-shrink-0 hidden w-56 mr-6 top-16 md:block"
-      style={{ filter: 'drop-shadow(4px 6px 4px #00000028)' }}
+      style={{ filter: "drop-shadow(4px 6px 4px #00000028)" }}
     >
       <TearedEdge direction="up" className="hidden md:block" />
 
@@ -37,6 +49,7 @@ const SidebarNav = ({ categories }) => {
         style={{ backgroundImage: `url(${linedPaper})` }}
       >
         <h1 className="text-xl font-bold text-gray-700">Navigacija</h1>
+
         <div className="flex flex-col mt-6">
           {categories.map(({ category }) => (
             <SidebarLink category={category} key={category} />
