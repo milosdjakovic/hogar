d49eab95b211c97d71f8ccee10f184aec9c73f37
Milos Djakovic <milos.g.djakovic@gmail.com>
2021-08-09 01:06:45 +0200

    Header active link hover background color fix

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -20,7 +20,7 @@ const Header = () => {
               transition: "background 300ms, color 300ms, box-shadow 300ms",
             }}
             className="flex items-center h-full px-4 hover:shadow-bottom-orange hover:bg-orange-200"
-            activeClassName="shadow-bottom-orange bg-orange-600 text-orange-100 hover:text-white"
+            activeClassName="shadow-bottom-orange bg-orange-600 hover:bg-orange-600 text-orange-100 hover:text-white"
             to="/"
           >
             <span className="whitespace-nowrap">Hogar 1</span>
@@ -30,7 +30,7 @@ const Header = () => {
               transition: "background 300ms, color 300ms, box-shadow 300ms",
             }}
             className="flex items-center h-full px-4 hover:shadow-bottom-orange hover:bg-orange-200"
-            activeClassName="shadow-bottom-orange bg-orange-600 text-orange-100 hover:text-white"
+            activeClassName="shadow-bottom-orange bg-orange-600 hover:bg-orange-600 text-orange-100 hover:text-white"
             to="/2/"
           >
             <span className="whitespace-nowrap">Hogar 2</span>
